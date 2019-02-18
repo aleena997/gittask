85d5773a009e60481e1b415697264fe1fc877b5c
aleenaanil997@gmail.com <aleenaanil997@gamil.com>
2019-02-18 11:43:54 +0530

    storing output to csv file

---
+++ settings.py
@@ -0,0 +1,10 @@
+BOT_NAME = 'sample'
+
+SPIDER_MODULES = ['sample.spiders']
+NEWSPIDER_MODULE = 'sample.spiders'
+
+FEED_FORMAT = "csv"
+FEED_URI ="recipe.csv"
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
